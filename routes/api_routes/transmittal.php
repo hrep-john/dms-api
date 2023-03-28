6024837d735af80aa957703ada204f10656dba43
akosinald <ronald.andres@gmail.com>
2023-03-28 13:31:42 +0800

    print transmittals end-point 2

--- routes/api_routes/transmittal.php
+++ routes/api_routes/transmittal.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\TransmittalController;
 
 Route::get('transmittal/{id}', [TransmittalController::class, 'index'])->name('transmittal.index');
 Route::post('transmittal/upload', [TransmittalController::class, 'upload'])->name('transmittal.upload');
+Route::post('transmittal/print-transmittal/{id}', [TransmittalController::class, 'print'])->name('transmittal.print');
