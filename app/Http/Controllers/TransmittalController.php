33c6a0451ee497c669c57c96a6e810e4d8419b4a
akosinald <ronald.andres@gmail.com>
2023-03-28 11:42:25 +0800

    document audit logs for transmittals

--- app/Http/Controllers/TransmittalController.php
+++ app/Http/Controllers/TransmittalController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use ApiErrorResponse;
 use App\Models\Transmittal;
+use App\Models\Document;
 use App\Traits\ApiResponder;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\Transmittal\StoreRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Storage;
+use Event;
 
 class TransmittalController extends Controller
 {
@@ -51,21 +53,44 @@ class TransmittalController extends Controller
 
         $user = Auth::user();
 
-        $transmittal = Transmittal::where('document_id', $request->document_id)->first();
+        $document_id = $request->document_id;
+
+        $transmittal = Transmittal::where('document_id', $document_id)->first();
         if ($transmittal) {
             $transmittal->update([
                 'transmittal_url' => $fullPath,
                 'updated_by' => $user->id
             ]);
+
+            $this->writeDocumentAuditLog($document_id, 'updated transmittal');
         } else {
             Transmittal::create([
-                'document_id' => $request->document_id,
+                'document_id' => $document_id,
                 'transmittal_url' => $fullPath,
                 'created_by' => $user->id,
                 'updated_by' => $user->id
             ]);
+
+            $this->writeDocumentAuditLog($document_id, 'uploaded transmittal');
         }
 
         return $this->success(['file_path' => $fullPath], Response::HTTP_OK);
+    }
+
+    public function print(int $document_id)
+    {
+        $this->writeDocumentAuditLog($document_id, 'printed transmittal');
+    }
+
+    public function writeDocumentAuditLog($document_id, $event, $old = [], $new = [])
+    {
+        $document = Document::find($document_id);
+
+        $document->auditEvent = $event;
+        $document->isCustomEvent = true;
+        $document->auditCustomOld = $old;
+        $document->auditCustomNew = $new;
+
+        Event::dispatch(AuditCustom::class, [$document]);
     }
 }
