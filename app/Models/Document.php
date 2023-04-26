19167cebd8a754ce27510e0c5826ed879ac91be6
akosinald <ronald.andres@gmail.com>
2023-04-26 17:28:52 +0800

    debug UDF 5

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Str;
 use OwenIt\Auditing\Models\Audit;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-use App\Mail\ExceptionOccured;
-use Mail;
+// use App\Mail\ExceptionOccured;
+// use Mail;
 
 class Document extends BaseModel implements HasMedia
 {
@@ -206,25 +206,25 @@ class Document extends BaseModel implements HasMedia
     {
         $udfs = App::make(UserDefinedFieldServiceInterface::class)->all(false);
 
-        $content['message'] = $udfs . ' - filename; ' . $this->file_name;
-        $content['url'] = request()->url();
-        $content['body'] = request()->all();
-        $content['ip'] = request()->ip();
-        Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
+        // $content['message'] = $udfs . ' - filename; ' . $this->file_name;
+        // $content['url'] = request()->url();
+        // $content['body'] = request()->all();
+        // $content['ip'] = request()->ip();
+        // Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
 
         $flattenData = [];
 
         $currentValue = JSON_DECODE($this->user_defined_field, true);
         
-        $content['message'] = $this->user_defined_field . ' - filename; ' . $this->file_name;
-        Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
+        // $content['message'] = $this->user_defined_field . ' - filename; ' . $this->file_name;
+        // Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
 
         foreach($udfs as $udf) {
             $flattenData[$udf->key] = $currentValue[$udf->key] ?? null;
         }
 
-        $content['message'] = JSON_ENCODE($flattenData) . ' - filename; ' . $this->file_name;
-        Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
+        // $content['message'] = JSON_ENCODE($flattenData) . ' - filename; ' . $this->file_name;
+        // Mail::to('ronald.andres@gmail.com')->send(new ExceptionOccured($content));
 
         return $flattenData;
     }
